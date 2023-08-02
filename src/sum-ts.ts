be88c950ee260bfed19ab0013965e479c58abe00
Matthew Chau <matthewwchau@gmail.com>
2023-08-02 15:05:57 -0700

    Included typescript scripts and created tests for 'add' in typescript.

---
+++ src/sum-ts.ts
@@ -0,0 +1,7 @@
+// export const add = (a,b) => {
+//     a + b;
+// }
+
+export function add(a: number, b: number) {
+    return a + b;
+}
--- test/sum-test.js
+++ test/sum-test.js
@@ -1,18 +1,39 @@
 // importing assert from Chai
-import { assert } from "chai";
+import { assert, expect } from "chai";
 // import function `add` from app.js
 import { add } from "../src/sum.js";
 
-describe('add function(2,2)', () => {
-    it('adds two numbers together', () => {
+// describe('add function(2,2)', () => {
+//     it('adds two numbers together', () => {
+//         const result = add(2, 2);
+//         assert.equal(result, 4);
+//     })
+// })
+
+// describe('add function(4,11)', () => {
+//     it('adds two numbers together', () => {
+//         const result = add(4, 11);
+//         assert.strictEqual(result, 15);
+//     })
+// })
+
+describe('add(a, b)', () => {
+    beforeEach(() => {
+        expect(add).to.be.a('function');
+    })
+
+    it('adds two numbers together (2, 2)', () => {
         const result = add(2, 2);
         assert.equal(result, 4);
     })
-})
 
-describe('add function(4,11)', () => {
-    it('adds two numbers together', () => {
+    it('adds two numbers together (4, 11)', () => {
         const result = add(4, 11);
         assert.strictEqual(result, 15);
+    })
+
+    it('adds two numbers together (5, 1)', () => {
+        const result = add(5, 1);
+        assert.strictEqual(result, 6);
     })
 })
