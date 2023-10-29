7a5e79a71e584677601c8efd271ef11c8399101e
Matthew Chau <matthewwchau@gmail.com>
2023-10-28 18:54:11 -0700

    working on algo for division.

--- src/division-ts.ts
+++ src/division-ts.ts
@@ -2,5 +2,19 @@
 
 
 export function divide(a:any, b:any) {
-    return (a/b);
+    // return (a/b);
+
+    // console.log(a.typeof)
+
+    // TEST
+    // if (a.typeof != Number || b.typeof != Number) {
+    //     return NaN;
+    // }
+
+    // if (typeof a != Number )
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+
 }
